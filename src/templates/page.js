df09d954218af564449ee7ebfc7a68f64465d87e
janosh <janosh.riebesell@gmail.com>
2019-02-17 21:16:31 +0000

    feat: page template query for cover image

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
 
-import Global from '../components/Global'
-import PageTitle from '../components/PageTitle'
-import PageBody from '../components/styles/PageBody'
+import Global from "../components/Global"
+import PageTitle from "../components/PageTitle"
+import PageBody from "../components/styles/PageBody"
 
-const PageTemplate = ({ data: { page }, location }) => {
-  const { title, body } = page
-  const { excerpt, html } = body && body.data
+const PageTemplate = ({ data, location }) => {
+  const { title, subtitle, body, cover, fullHeightCover } = data.page
+  const { excerpt, html } = (body && body.remark) || subtitle.remark
   return (
-    <Global pageTitle={title} path={location.pathname} description={excerpt}>
+    <Global
+      pageTitle={title}
+      path={location.pathname}
+      description={excerpt}
+      cover={{ ...cover, fullHeight: fullHeightCover }}
+    >
       <PageTitle title={title} />
       {html && <PageBody dangerouslySetInnerHTML={{ __html: html }} />}
     </Global>
@@ -19,16 +24,31 @@ const PageTemplate = ({ data: { page }, location }) => {
 export default PageTemplate
 
 export const query = graphql`
+  fragment pageFields on ContentfulPage {
+    title
+    subtitle {
+      remark: childMarkdownRemark {
+        excerpt
+        html
+      }
+    }
+    slug
+    body {
+      remark: childMarkdownRemark {
+        excerpt
+        html
+      }
+    }
+    cover {
+      fluid {
+        src
+      }
+    }
+    fullHeightCover
+  }
   query($slug: String!) {
     page: contentfulPage(slug: { eq: $slug }) {
-      title
-      slug
-      body {
-        data: childMarkdownRemark {
-          excerpt
-          html
-        }
-      }
+      ...pageFields
     }
   }
 `
