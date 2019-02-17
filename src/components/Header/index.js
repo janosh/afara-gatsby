da441384d14c022c74a4efb0feb089303c9fc694
janosh <janosh.riebesell@gmail.com>
2019-02-17 20:07:49 +0000

    feat: add Social to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react"
 
-import { HeaderContainer, SiteTitle } from './styles'
-import Nav from '../Nav'
-import Search from '../Search'
-import Logo from '../../assets/logo'
+import { HeaderContainer, SiteTitle } from "./styles"
+import Nav, { navLinkStyle } from "../Nav"
+import Social from "../Social"
+import Search from "../Search"
+import Logo from "../../assets/logo"
 
 const searchIndices = [
-  { name: `Pages`, title: `Pages`, hitComp: `PageHit` },
+  { name: `Pages`, title: `Seiten`, hitComp: `PageHit` },
   { name: `Posts`, title: `Blog Posts`, hitComp: `PostHit` },
 ]
 
 const Header = ({ site }) => (
   <HeaderContainer>
     <SiteTitle to="/" title={site.title} rel="home">
-      <Logo />
+      <Logo alt={site.title} />
     </SiteTitle>
     <Nav />
+    <Social collapse linkStyle={navLinkStyle} />
     <Search collapse indices={searchIndices} />
   </HeaderContainer>
 )
