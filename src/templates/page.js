67d2fa20d0deb4fbf486ea529313724de4b6c44a
janosh <janosh.riebesell@gmail.com>
2019-02-27 19:19:20 +0000

    feat: page template add Slideshow or Img as background prop to PageTitle

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,21 +1,32 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Global from "../components/Global"
 import PageTitle from "../components/PageTitle"
+import Slideshow from "../components/Slideshow"
 import PageBody from "../components/styles/PageBody"
 
 const PageTemplate = ({ data, location }) => {
-  const { title, subtitle, body, cover, fullHeightCover } = data.page
+  const { title, subtitle, body, cover, slideshow, titleHeight } = data.page
   const { excerpt, html } = (body && body.remark) || subtitle.remark
+  const background = slideshow ? (
+    <Slideshow>
+      {slideshow.slides.map(({ title, image }) => (
+        <Img key={title} fluid={image.fluid} />
+      ))}
+    </Slideshow>
+  ) : cover ? (
+    <Img fluid={cover.fluid} />
+  ) : null
   return (
-    <Global
-      pageTitle={title}
-      path={location.pathname}
-      description={excerpt}
-      cover={{ ...cover, fullHeight: fullHeightCover }}
-    >
-      <PageTitle title={title} />
+    <Global pageTitle={title} path={location.pathname} description={excerpt}>
+      <PageTitle background={background} height={titleHeight}>
+        <h1>{title}</h1>
+        {subtitle && (
+          <h2 dangerouslySetInnerHTML={{ __html: subtitle.remark.html }} />
+        )}
+      </PageTitle>
       {html && <PageBody dangerouslySetInnerHTML={{ __html: html }} />}
     </Global>
   )
