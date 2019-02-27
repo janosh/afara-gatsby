1906447515e777ce534acb43ddc02becc9d60285
janosh <janosh.riebesell@gmail.com>
2019-02-27 19:15:32 +0000

    chore: add contentful management script

---
+++ src/utils/contentful.js
@@ -0,0 +1,24 @@
+const contentful = require(`contentful-management`)
+require(`dotenv`).config()
+
+const client = contentful.createClient({
+  accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+})
+
+client
+  .getSpace(process.env.CONTENTFUL_SPACE_ID)
+  .then(space => space.getEnvironment(`master`))
+  .then(env => env.getEntries())
+  .then(response =>
+    response.items.filter(item => item.fields.slug && item.fields.slug.de)
+  )
+  .then(items =>
+    items.forEach(item => {
+      item.fields.slug.de = (`/` + item.fields.slug.de).replace(`//`, `/`)
+      item.update()
+      return item.publish()
+    })
+  )
+  /* eslint-disable no-console */
+  .then(console.log)
+  .catch(console.error)
