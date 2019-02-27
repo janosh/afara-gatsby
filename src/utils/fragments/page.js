48c171ec9d2b2db42fac15abad3dd238a74e1abf
janosh <janosh.riebesell@gmail.com>
2019-02-27 19:14:15 +0000

    refactor: outsource graphql fragments to src/utils/fragments

---
+++ src/utils/fragments/page.js
@@ -0,0 +1,36 @@
+import { graphql } from "gatsby"
+
+export const query = graphql`
+  fragment pageFields on ContentfulPage {
+    title
+    subtitle {
+      remark: childMarkdownRemark {
+        excerpt
+        html
+      }
+    }
+    slug
+    body {
+      remark: childMarkdownRemark {
+        excerpt
+        html
+      }
+    }
+    cover {
+      fluid {
+        ...GatsbyContentfulFluid_withWebp
+      }
+    }
+    slideshow {
+      slides {
+        title
+        image {
+          fluid(quality: 90, maxWidth: 1500) {
+            ...GatsbyContentfulFluid_withWebp
+          }
+        }
+      }
+    }
+    titleHeight
+  }
+`
